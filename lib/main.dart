658d3feefeeb2c0dd06c0ca210e78969fa1abca9
Angelin_Agustin_2226240035 <angelinagustin_2226240035@mhs.mdp.ac.id>
2025-02-26 05:10:25 +0000

    add : queiowq

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,78 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:karyawan/models/karyawan.dart';
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: "Daftar Karyawan",
       theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key});
+  Future<List<Karyawan>> _readJsonData() async {
+    String response = await rootBundle.loadString("assets/karyawan.json");
+    final List<dynamic> data = json.decode(response);
+    return data.map((json) => Karyawan.fromJson(json)).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Daftar Pegawai"),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      body: FutureBuilder<List<Karyawan>>(
+          future: _readJsonData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      snapshot.data![index].nama,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Umur: ${snapshot.data![index].umur} tahun"),
+                        Text("Alamat: ${snapshot.data![index].alamat.jalan},"
+                            "${snapshot.data![index].alamat.kota}, "
+                            "${snapshot.data![index].alamat.provinsi}"),
+                        // tampilkan hobi
+                        if (snapshot.data![index].hobi.length > 1)
+                          Text("Hobby : ${snapshot.data![index].hobi}"),
+                      ],
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            }
+            return const Center(child: CircularProgressIndicator());
+          }),
     );
   }
 }
